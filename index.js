b96769297f92fef4009cbbab553ffccd34e17f63
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-13 15:06:54 +0800

    Update project

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ document.getElementById('faq-btn').addEventListener('click', function () {
     document.getElementById('frequently-section').style.display = "block";
     document.getElementById('crad-section').style.display = "none";
     document.getElementById('Lets-Learn').style.display = "none";
+    document.getElementById('hero-section').style.display = "none";
 
 });
