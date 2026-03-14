3e49b702e98d9127972c594114ce41f6201608bf
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-14 14:53:20 +0800

    Update project

--- index2.js
+++ index2.js
@@ -1,20 +1,19 @@
-const gretStratbtn = document.getElementById("loginBtn")
+const gretStratbtn = document.getElementById("loginBtn");
 
-gretStratbtn.addEventListener("click", function(){
+gretStratbtn.addEventListener("click", function () {
+    // .trim() use korle space thakleo problem hobe na
+    const inpuNmae = document.getElementById("nameinput").value.trim();
+    const inpuPassword = document.getElementById("passwordinput").value.trim();
 
-const inpuNmae =document.getElementById("nameInput").value
-const inpuPassword =document.getElementById("passwordInput").value
-
-if(inpuNmae === " "){
-    alert("Please Enter Your name");
-    return;
-};
-
-if(inpuPassword !== ("123456") ){
-     alert("Password Must be 123456");
-     return;
-}
-
-alert("login successful")
-
+    if (inpuNmae === "masud" && inpuPassword === "123456") {
+        alert("Login successful");
+        // Ekhane apni chaile onno page-e niye jete paren
+        // window.location.href = "dashboard.html"; 
+    }
+    else if (inpuNmae !== "masud") {
+        alert("Please enter the correct name"); // Console-er poriborte Alert
+    }
+    else {
+        alert("Password must be 123456");
+    }
 });
