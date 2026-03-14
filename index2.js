9bc93bdb14bc328677346258605693c4a2bb4b15
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-15 01:33:31 +0800

    Update project

--- index2.js
+++ index2.js
@@ -1,37 +1,4 @@
-const gretStratbtn = document.getElementById("login-btn");
-document.getElementById("navbar").style.display = "none"
 
-// login section 
-gretStratbtn.addEventListener("click", function () {
-    // .trim() use korle space thakleo problem hobe na
-    const inpuNmae = document.getElementById("name-input").value.trim();
-    const inpuPassword = document.getElementById("password-input").value.trim();
-
-    if (inpuNmae === "masud" && inpuPassword === "123456") {
-
-
-        document.getElementById("hero-section").style.display = "none"
-        document.getElementById("navbar").style.display = "block"
-        alert("Login successful");
-        // Ekhane apni chaile onno page-e niye jete paren
-        // window.location.href = "dashboard.html"; 
-
-
-    }
-    else if (inpuNmae !== "masud") {
-        alert("Please enter the correct name"); // Console-er poriborte Alert
-    }
-    else {
-        alert("Password must be 123456");
-    }
-
-
-
-
-    // document.getElementById("navbar").classList.remove("hidden")
-    // document.getElementById("vocabulary").classList.remove("hidden")
-    // document.getElementById("faq").classList.remove("hidden")
-});
 
 
 // Logout button cliked function 
