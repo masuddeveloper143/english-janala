3031490a97faebc9ad42788c900ea8a048cc52fb
Masud Rana <md.masudrana.developer@gmail.com>
2026-03-15 22:50:09 +0800

    Update project

--- index2.js
+++ index2.js
@@ -68,17 +68,13 @@ function displayWords(words) {
     container.innerHTML = ""
 
     if (words.length === 0) {
-        container.innerHTML = `<!-- Open the modal using ID.showModal() method -->
-<button class="btn" onclick="my_modal_2.showModal()">data not found</button>
-<dialog id="my_modal_2" class="modal">
-  <div class="modal-box">
-    <h3 class="text-lg font-bold">Hello!</h3>
-    <p class="py-4">Press ESC key or click outside to close</p>
-  </div>
-  <form method="dialog" class="modal-backdrop">
-    <button>close</button>
-  </form>
-</dialog>`
+        container.innerHTML = `
+<div class=" flex justify-center items-center">
+
+        <img class="w-40 " src="./assets/alert-error.png" alt="">
+
+    </div>
+`;
         return
     }
 
